2db717dd3fca33e1ec1886e634ff4fed6f9bee1a
User <oriana.flores@ucb.edu.bo>
2026-03-02 16:05:10 -0400

    fix: jugador 2 anota 1

--- src/tennis.js
+++ src/tennis.js
@@ -1,29 +1,35 @@
 class Tennis {
-    constructor() {
-        this.player1Points = 0;
+  constructor() {
+    this.player1Points = 0;
+    this.player2Points = 0;
+  }
+
+  score() {
+    if (this.player1Points === 4) {
+      return "Game for player 1";
     }
-    score(){
-        if (this.player1Points === 4) {
-            return "Game for player 1";
-        }
-        
-        let player1Score = "Love";
-        if(this.player1Points == 1){
-            player1Score= "15"
-        }
-        if(this.player1Points == 2){
-            player1Score= "30"
-        }
-        if(this.player1Points == 3){
-            player1Score= "40"
-        }
-        
-        return player1Score+" - Love"
+    if (this.player2Points === 4) {
+      return "Game for player 2";
     }
 
-    player1Scores(){
-        this.player1Points++;
-    }
+    let player1Score = "Love";
+    if (this.player1Points == 1) player1Score = "15";
+    if (this.player1Points == 2) player1Score = "30";
+    if (this.player1Points == 3) player1Score = "40";
+
+    let player2Score = "Love";
+    if (this.player2Points == 1) player2Score = "15";
+
+    return player1Score + " - " + player2Score;
+  }
+
+  player1Scores() {
+    this.player1Points++;
+  }
+
+  player2Scores() {
+    this.player2Points++;
+  }
 }
 
 export default Tennis;
